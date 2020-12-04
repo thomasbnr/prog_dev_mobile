14a8d73faa2397eb7f320d950d9182d2165d18d9
ThomasB <th.bonnerot@gmail.com>
2020-12-04 19:14:11 +0100

    add collision

--- DinosaurGame/js/test.js
+++ DinosaurGame/js/test.js
@@ -1,37 +1,37 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-var fps, Play, sprite,sol, cloud, trex, cactus;
+let fps, Play, sprite, sol, cloud, trex, cactus, fin,score = 0;
 const velocity = 5;
-
 const init = () => {
+
 
     // SPRITE
     sprite = new Image();
     sprite.src = "../assets/sprite.png";
-    let score = 0;
+
     // SOL
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/createPattern
     sol = {
         x: 0,y: 220,
         draw() {
-            var pattern = ctx.createPattern(sprite, "repeat-x");
+            var pat = ctx.createPattern(sprite, "repeat-x");
             ctx.save();
             ctx.translate(this.x, 100);
-            ctx.fillStyle = pattern;
-            ctx.fillRect(0, 100, sprite.width * 1000, sprite.height); //sprite.width * 1000 --> pour que le sol défile en continu
+            ctx.fillStyle = pat;
+            ctx.fillRect(0, 100, sprite.width * 1000, sprite.height);
             ctx.restore();
             // MOVE
             this.x -= velocity;
+
             score ++
             ctx.font = "10px arial";
             ctx.fillStyle = "black";
             ctx.fillText("Score : " + score, 380, 20);
         }
     }
-
-
-    // Nuages placés de manière aléatoire en y dans le canvas, 1 nuage dans le canvas
+    // CLOUD
+    // Mes sprites sont sur une seul image, donc je choisie  celui que je veux
+    // Il peut y avoir qu'un seul nuage dans le canvas qui apparaît alétoirement en fonction de y avec le même x
     cloud = {
         w: 100,h: 100,sx: 100,copy: [],
         create() {
@@ -50,6 +50,7 @@ const init = () => {
             }
         },
         draw() {
+            // CREATE
             this.create();
             // DRAW
             for (i = 0; i < this.copy.length; i++) {
@@ -59,14 +60,11 @@ const init = () => {
             }
         }
     }
-    // T-REX && cactus
+    // T-REX
     trex = {
-        cactus_w: 45, cactus_h: 45, cactus_sx : 500, cactus_copy: [],
-
-        //T-Rex
-        x: 20,y: 0,w: 50,h: 50,yv: 0,sx: 100 * 7,sy: 0,sw: 100,sh: 100,jumping: false,onGround: false, col: false,
-        animation: {scene: 0,ready: true,delais: 50,
-        //On récupèrent les sprites dont on a besoin pour faire nos animations
+        x: 100,y: 0,w: 50,h: 50,yv: 0,sx: 100 * 7,sy: 0,sw: 100,sh: 100,jumping: false,onGround: false,col: false,
+        animation: {scene: 0,ready: true,delay: 50,
+        // 3 différentes animations quand il cours
         run: [
             [100 * 7],[100 * 8],[100 * 9]],
             jump: 100 * 7,
@@ -84,73 +82,37 @@ const init = () => {
                         }
                         trex.sx = this.run[this.scene];
                     }
-                    // JUMPING
                     if (trex.jumping) {
                         trex.sx = this.jump;
                     }
-                    // RESET
                     setTimeout(() => {
                         this.ready = true;
-                    }, this.delais);
+                    }, this.delay);
                 }
             }
         },
-        create() {
-            // COndition pour le premier cactus
-            if (this.cactus_copy.length == 0) {
-                this.cactus_copy.push({
-                    cactus_x: canvas.width,
-                    cactus_y: 170
-                });
-            } else {
-                // Spawn d'un cactus en x quand il n'a plus de cactus dans le canvas
-                if (this.cactus_copy[this.cactus_copy.length - 1].cactus_x < 0) {
-                    this.cactus_copy.push({
-                        cactus_x: canvas.width,
-                        cactus_y: 170
-                    });
-                }
-            }
-        },
-
         draw() {
-            this.create();
-            // On le crée puis le dessine
-            let compteur = 0;
-            let speed = 5;
-            for (i = 0; i < this.cactus_copy.length; i++) {
-                ctx.drawImage(sprite, this.cactus_sx, 0, 100, 100, this.cactus_copy[i].cactus_x, this.cactus_copy[i].cactus_y, this.cactus_w, this.cactus_h);
-                // MOVE
-                this.cactus_copy[i].cactus_x -= speed;
-                compteur++;
-
-                // Niveau de difficulté --> à transformer en switch case --> plus propres
-                if(compteur > 10)
-                    this.cactus_copy[i].cactus_x -= speed -4;
-                if (compteur >25)
-                    this.cactus_copy[i].cactus_x -= speed-3;
-                if (compteur > 40)
-                    this.cactus_copy[i].cactus_x -= speed-2.8;
-            }
+            // UPDATE
             this.animation.animate();
             this.move();
             this.collision();
             // DRAW
             ctx.drawImage(sprite, this.sx, this.sy, this.sw, this.sh, this.x, this.y, this.w, this.h);
+            //console.log(this.x);
         },
         move() {
-            // evenement saut avec la barre d'espace
-            onkeydown = function(event) { //--> cheat à enlever 
+            // Evènements saut avec la barre d'espace
+            onkeydown = (e) => {
                 // JUMP
-                if (event.keyCode == 32 && trex.onGround) {
+                if (e.keyCode == 32 && trex.onGround) {
                     trex.onGround = false;
                     trex.jumping = true;
-                    trex.col = false;
                     trex.yv = -11;
                 }
             }
-            onkeyup = function(event) {
-                if (event.keyCode == 32) {
+            onkeyup = (e) => {
+                // JUMP
+                if (e.keyCode == 32) {
                     trex.jumping = false;
                 }
             }
@@ -158,18 +120,63 @@ const init = () => {
             if (!this.onGround) {
                 this.yv += 0.5;
             }
+            // UPDATE
             this.y += this.yv;
         },
         collision() {
-            // Collison pour le sol
+            // collison trex && sol
             if (this.y + this.h > sol.y) {
                 this.y = sol.y - this.h;
                 this.onGround = true;
             }
         }
     }
+    // Cactus
+    cactus = {
+        y : 170,w: 45, h: 45, sx : 500, copy: [],
+        create() {
+            if (this.copy.length == 0) {
+                this.copy.push({
+                    x: canvas.width,
+                    y: 170
+                });
+            } else {
+                if (this.copy[this.copy.length - 1].x < 0) {
+                    this.copy.push({
+                        x: canvas.width,
+                        y: 170
+                    });
+                }
+            }
+        },
+        draw() {
+            // CREATE
+            this.create();
+            // DRAW
+            let compteur = 0;
+            for (i = 0; i < this.copy.length; i++) {
+                ctx.drawImage(sprite, this.sx, 0, 100, 100, this.copy[i].x, this.copy[i].y, this.w, this.h);
+                compteur++;
+                // MOVE
+                this.copy[i].x -= 5;
+               if(((trex.x + trex.w-9 >= this.copy[i].x) && (trex.x <= this.copy[i].x + this.w-10)) && ((trex.y + trex.h-10 >= this.copy[i].y+10) && (trex.y <= this.copy[i].y+10 + this.h))) {
+                console.log("col");
+                Play = false;
+               }
+            }
+        }
+    },
+    fin = {
+        x : 200, y : 50, w:50, h: 50, sx:0, sy :0,sw : 100, sh: 100,
+        draw() {
+            ctx.drawImage(sprite, this.sx,this.sy, this.sw, this.sh, this.x, this.y, this.w, this.h);
 
-    // boucle principale
+            ctx.font = "10px arial";
+            ctx.fillStyle = "black";
+            ctx.fillText("Appuyer sur F5 pour recommencer", 145, 95);
+        }
+    }
+    // Boucle principale
     fps = 60;
     Play = true;
     setInterval(() => {
@@ -180,11 +187,16 @@ const init = () => {
 init();
 const update = () => {
     if (Play) {
-        // Obligatoire pour effacer le canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // on déssine le sol, les nuages puis cactus et trex
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacement du canvas --> Obligatoire
+
+        // DRAW
         sol.draw();
         cloud.draw();
         trex.draw();
+        cactus.draw();
+
+    }
+    else {
+        fin.draw();
     }
 }
