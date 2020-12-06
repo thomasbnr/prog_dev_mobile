0251162575d8077b7bcb3a285fa23710afe3be77
ThomasB <th.bonnerot@gmail.com>
2020-12-06 17:56:18 +0100

    update

--- camera/js/main.js
+++ camera/js/main.js
@@ -10,25 +10,25 @@ const resize = () => {
 const createCube = (color, x, y, z) => {
 
 	// create cube geom and material
-	var geometry = new THREE.BoxGeometry();
-	var material = new THREE.MeshBasicMaterial( { color: color } );
-	var cube = new THREE.Mesh( geometry, material );
+	let geometry = new THREE.BoxGeometry();
+	let material = new THREE.MeshBasicMaterial( { color: color } );
+	let cube = new THREE.Mesh( geometry, material );
 	return cube;
 };
 
 const createTonus = (color, x, y, z) => {
 
-	var geometry = new THREE.TorusGeometry ();
-	var material = new THREE.MeshBasicMaterial( { color: 0x0000FF  } );
-	var torus = new THREE.Mesh( geometry, material );
+	let geometry = new THREE.TorusGeometry ();
+	let material = new THREE.MeshBasicMaterial( { color: 0x0000FF  } );
+	let torus = new THREE.Mesh( geometry, material );
 	return torus;
 };
 
 const createPlane = (color,x,y,z) => {
 
-	var geometry = new THREE.PlaneGeometry( 4, 5, 5 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00FFFF, side: THREE.DoubleSide} );
-	var plane = new THREE.Mesh( geometry, material );
+	let geometry = new THREE.PlaneGeometry( 4, 5, 5 );
+	let material = new THREE.MeshBasicMaterial( {color: 0x00FFFF, side: THREE.DoubleSide} );
+	let plane = new THREE.Mesh( geometry, material );
 	return plane;
 
 };
@@ -38,48 +38,89 @@ const main = () => {
 	console.log("hello world");
 
 	// initialisation de la scène
-	var scene = new THREE.Scene();
+	let scene = new THREE.Scene();
 
 	// init camera
-	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+	let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 
 	// web gl renderer
-	var renderer = new THREE.WebGLRenderer();
+	let renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
 
 	// FPS command
-	let controls = new THREE.FirstPersonControls(camera, renderer.domElement);
-	var clock = new THREE.Clock();
-	controls.lookSpeed = 0.1;
+
+	let controls = new THREE.PointerLockControls(camera, renderer.domElement);
+	document.body.appendChild( renderer.domElement );
 
 
-	var cube = createCube("#FF0000");
+	//	let clock = new THREE.Clock();
+//	controls.lookSpeed = 0.1;
+
+
+	let cube = createCube("#FF0000");
 	// add cube to scene
 	scene.add( cube );
 
-	var recube = createCube("#00FF00");
+	let recube = createCube("#00FF00");
 	// add cube to scene
 	scene.add( recube );
 
-	var torus = createTonus();
+	let torus = createTonus();
 	scene.add (torus);
 
-	var plane = createPlane();
+	let plane = createPlane();
 	scene.add (plane);
 
 	// Je donne des positions à mes formes
 	plane.position.set(2,2,2);
 	torus.position.set(0,0,0);
 
+
 	camera.position.z = 10;
+
+	window.addEventListener('keydown',keydown_fun, false);
+	function keydown_fun(e){
+		switch(e.code) {
+			case "KeyV" :
+				controls.moveRight(-0.5);
+				console.log("VV");
+				break;
+			case "KeyE" :
+				controls.moveRight(0.5);
+				console.log("EEE");
+				break;
+
+			case "KeyN" :
+				controls.moveForward(0.5);
+				console.log("H");
+				break;
+
+			case "KeyS" :
+				controls.moveForward(-0.5);
+				console.log("Sss")
+				break;
+
+			case "KeyL" :
+				controls.lock();
+				break;
+
+			case "KeyU" :
+				controls.unlock();
+				break;
+		}
+	}
 
 	animate();
 
 	// animate loop
 	function animate() {
 		requestAnimationFrame( animate ); // request next frame
-		var delta = clock.getDelta();
+		//let delta = clock.getDelta();
+
+
 		// move cube
 
 		cube.rotation.x += 0.01;
@@ -96,10 +137,11 @@ const main = () => {
 		recube.rotation.x -= 0.01;
 		recube.rotation.y -= 0.01;
 
-		controls.update(delta);
+	//	controls.update(delta);
 
 		// render !
 		renderer.render( scene, camera );
+
 	}
 
 };
